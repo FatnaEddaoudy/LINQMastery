e666da4143e058c206011b8237ca275b2ce370dc
Fatna Eddaoudy <143269693+FatnaEddaoudy@users.noreply.github.com>
2025-10-20 17:35:02 +0200

    Delete PortfolioProjects/Pizza-Ordering-System directory

--- PortfolioProjects/Pizza-Ordering-System/Online Pizza Ordering System/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Online_Pizza_Ordering_System.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- PortfolioProjects/Pizza-Ordering-System/Online Pizza Ordering System/Models/Size.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Online_Pizza_Ordering_System.Models
-{
-    public enum  Size
-    {
-        Small,
-        Medium,
-        Large
-    }
-}
